cd1e0b2e339c3f85c7d486a371ba90ffda1fdfa2
yarocruz <yarocruz@gmail.com>
2022-02-13 19:50:18 -0500

    added terminion crate, added ctrl q to quit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::io::{self, stdout, Read};
+use termion::raw::IntoRawMode;
+
+fn to_ctrl_byte(c: char) -> u8 {
+    let byte = c as u8;
+    byte & 0b0001_1111
+}
+
+fn terminate(e: std::io::Error) {
+    panic!(e);
+}
+
 fn main() {
-    println!("Hello from d-ario!");
+    let _stdout = stdout().into_raw_mode().unwrap();
+
+   for b in io::stdin().bytes() {
+       let b = b.unwrap();
+       let c = b as char;
+
+       //println!("{:#b}", b); the :#b prints the b's value in binary
+
+       if c.is_control() {
+           println!("{:?} \r", b); // the \r is "carriage return"
+       } else {
+           println!("{:?} ({})\r", b, c);
+       }
+
+       if b == to_ctrl_byte('q') {
+           break;
+       }
+   }
 }
