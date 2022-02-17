7ae1bc9a01f2204f2bcbe152c990b2645846e0e6
yarocruz <yarocruz@gmail.com>
2022-02-16 19:16:43 -0500

    added document and row structs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![warn(clippy::all, clippy::pedantic)]
+mod document;
 mod editor;
+mod row;
 mod terminal;
+pub use document::Document;
+pub use row::Row;
 pub use terminal::Terminal;
 pub use editor::Position;
 use editor::Editor;
