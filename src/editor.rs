a6bdd44ab6e9ba1227cc2a184eba84ac94503aa6
yarocruz <yarocruz@gmail.com>
2022-02-18 11:42:43 -0500

    made horizontal scrolling work

--- src/editor.rs
+++ src/editor.rs
@@ -60,7 +60,10 @@ impl Editor {
             println!("Goodbye.\r");
         } else {
             self.draw_rows();
-            Terminal::cursor_position(&self.cursor_position);
+            Terminal::cursor_position(&Position {
+                x: self.cursor_position.x.saturating_sub(self.offset.x),
+                y: self.cursor_position.x.saturating_sub(self.offset.y),
+            });
         }
         Terminal::cursor_show();
         Terminal::flush()
@@ -100,9 +103,12 @@ impl Editor {
     }
     fn move_cursor(&mut self, key: Key) {
         let Position { mut y, mut x } = self.cursor_position;
-        let size = self.terminal.size();
-        let height = size.height.saturating_sub(1) as usize;
-        let width = size.width.saturating_sub(1) as usize;
+        let height = self.document.len();
+        let width = if let Some(row) = self.document.row(y) {
+            row.len()
+        } else {
+          0
+        };
         match key {
             Key::Up => y = y.saturating_sub(1),
             Key::Down => {
