cb1c7d0e696f0c6a13be93f6bf27a3a045877476
yarocruz <yarocruz@gmail.com>
2022-02-19 14:53:11 -0500

    deleting with backspace appends to the next above row

--- src/document.rs
+++ src/document.rs
@@ -40,8 +40,17 @@ impl Document {
         }
     }
     pub fn delete(&mut self, at: &Position) {
-        if at.y >= self.len() {
+        let len = self.len();
+        if at.y >= len {
             return;
+        }
+        if at.x == self.rows.get_mut(at.y).unwrap().len() && at.y < len - 1 {
+            let next_row = self.rows.remove(at.y + 1);
+            let row = self.rows.get_mut(at.y).unwrap();
+            row.append(&next_row);
+        } else {
+            let row = self.rows.get_mut(at.y).unwrap();
+            row.delete(at.x);
         }
         let row = self.rows.get_mut(at.y).unwrap();
         row.delete(at.x);
